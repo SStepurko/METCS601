08d90140480d75b4397e0ed71bd8224771c9ffd3
SStepurko <82845341+SStepurko@users.noreply.github.com>
2021-11-30 08:34:17 +0300

    Update main.js

--- Assignments/CS601_HW4_Stepurko/main.js
+++ Assignments/CS601_HW4_Stepurko/main.js
@@ -1,5 +1,20 @@
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const facilitator = document.getElementById('facilitator');
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
-	e.preventDefault();
+	if (!fieldsValid()) {
+		e.preventDefault();
+	}
 });
+
+function fieldsValid() {
+	let isValid = false;
+
+	return isValid;
+}
+
+function isValidNames(str) {
+	let validRegex = 
+}
