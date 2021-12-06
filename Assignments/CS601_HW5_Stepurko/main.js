4d0bf1b4523f8261a4102018f197394b0fb6937d
SStepurko <82845341+SStepurko@users.noreply.github.com>
2021-12-06 22:26:19 +0300

    write dom

--- Assignments/CS601_HW5_Stepurko/main.js
+++ Assignments/CS601_HW5_Stepurko/main.js
@@ -3,9 +3,19 @@ let myRequest = new Request("./my_degrees.json");
 fetch(myRequest)
     .then(function(resp) {
         console.log(resp);
-        return resp.json();
+        if (resp.ok) {
+            return resp.json();
+        } else {
+            throw "error";
+        }
     })
     .then(function(data) {
+        const doc = document.getElementById("content");
+        // console.log(data["my_degree"][0]["institution"]);
+        for (key in data["my_degree"]) {
+            doc.innerHTML += JSON.stringify(data["my_degree"][key]["institution"]);
+            console.log(data["my_degree"][key]);
+        }
         console.log(data);
     })
     .catch(console.error())
