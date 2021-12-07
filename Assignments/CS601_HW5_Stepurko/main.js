f469e4d4d4f7fed90a41e9f09c3c552a476b5721
SStepurko <82845341+SStepurko@users.noreply.github.com>
2021-12-07 12:58:09 +0300

    final

--- Assignments/CS601_HW5_Stepurko/main.js
+++ Assignments/CS601_HW5_Stepurko/main.js
@@ -1,26 +1,45 @@
 let myRequest = new Request("./my_degrees.json");
+const btn = document.getElementById('btn');
+const doc = document.getElementById("myTable");
 
-fetch(myRequest)
-    .then(function(resp) {
-        console.log(resp);
-        if (resp.ok) {
-            return resp.json();
-        } else {
-            throw "error";
-        }
-    })
-    .then(function(data) {
-        const doc = document.getElementById("myTable");
+function run() {
+    fetch(myRequest)
+    .then(checkData)
+    .then(json)
+    .then(showData)
+    .catch(Error)
+}
+
+function checkData(response) {
+    if (response.status >= 200 && response.status < 300) {
+        btn.innerHTML = "ALL OK!";
+        btn.className = "btn btn-ok";
+        btn.disabled = true;
+        return Promise.resolve(response);
+      } else {
+        throw "Error";
+      }  
+}
+
+function json(response) {
+    return response.json()    
+}
+
+function showData(data) {
         for (key in data["my_degree"]) {
             let row = `<tr>
-                        <td>${data["my_degree"][key]["institution"]["School"]}</td>
-                        <td>${data["my_degree"][key]["institution"]["Program"]}</td>
-                        <td>${data["my_degree"][key]["institution"]["Type"]}</td>
-                        <td>${data["my_degree"][key]["institution"]["Year"]}</td>
-                      </tr>`;
+            <td>${data["my_degree"][key]["institution"]["School"]}</td>
+            <td>${data["my_degree"][key]["institution"]["Program"]}</td>
+            <td>${data["my_degree"][key]["institution"]["Type"]}</td>
+            <td>${data["my_degree"][key]["institution"]["Year"]}</td>
+            </tr>`;
             doc.innerHTML += row;
-            console.log(data["my_degree"][key]);
         }
         console.log(data);
-    })
-    .catch(console.error())
+}
+
+function showError(err) {
+    btn.innerHTML = "Error";
+    btn.className = "btn btn-err";
+    console.log("Request failed", err)
+}
